da4d91b1cb23e0d33d0386af2078b9370b3c56e5
Praney Behl <praneybehl@gmail.com>
2020-08-11 08:05:20 +1000

    Initial commit

---
+++ app2/dist/940.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_basic_host_remote_app2=window.webpackJsonp_basic_host_remote_app2||[]).push([[940],{940:(t,e,o)=>{"use strict";o.r(e),o.d(e,{default:()=>a});var p=o(369),s=o.n(p);const a=()=>s().createElement("button",{style:{background:"red",color:"white"}},"App 2 Button")}}]);
